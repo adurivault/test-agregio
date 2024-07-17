ccfee44518937a00acf5671b70c5c8f7d9c1c6bf
adurivault <a.durivault@gmail.com>
2024-07-17 11:19:23 +0200

    src sensors

--- sql/load_src.sql
+++ sql/load_src.sql
@@ -4,17 +4,21 @@ SET search_path TO SRC;
 --src_productions
 DROP TABLE IF EXISTS src_production; 
 CREATE TABLE src_production (
-production_id SERIAL PRIMARY KEY,
-site_id INT NOT NULL,
-sensor_id INT NOT NULL,
-production_date DATE,
-energy_generated DECIMAL(10, 2),
-energy_type VARCHAR(50)
+    production_id SERIAL PRIMARY KEY,
+    site_id INT NOT NULL,
+    sensor_id INT NOT NULL,
+    production_date DATE,
+    energy_generated DECIMAL(10, 2),
+    energy_type VARCHAR(50)
 );
 
 INSERT INTO src_production (site_id, sensor_id, production_date, energy_generated, energy_type)
 SELECT DISTINCT ON (site_id, sensor_id, production_date)
-    site_id, sensor_id, production_date, energy_generated, energy_type 
+    site_id, 
+    sensor_id, 
+    production_date, 
+    energy_generated, 
+    energy_type 
 FROM RAW.raw_production
 WHERE 
     site_id IS NOT NULL
@@ -61,3 +65,28 @@ SELECT
 FROM first_data
 LEFT JOIN last_data
 ON first_data.site_name = last_data.site_name;
+
+-- src_sensors
+DROP TABLE IF EXISTS src_sensors; 
+CREATE TABLE src_sensors (
+    sensor_id SERIAL PRIMARY KEY,
+    site_id INT NOT NULL,
+    sensor_type VARCHAR(50),
+    installation_date DATE,
+    status VARCHAR(50)
+);
+
+INSERT INTO src_sensors (sensor_id, site_id, sensor_type, installation_date, status)
+SELECT DISTINCT ON (sensor_id)
+    sensor_id, 
+    site_id, 
+    sensor_type, 
+    installation_date, 
+    status
+FROM RAW.raw_sensors
+WHERE 
+    sensor_id IS NOT NULL
+    AND site_id IS NOT NULL
+    AND sensor_type IS NOT NULL
+    AND installation_date IS NOT NULL
+    AND status IS NOT NULL;
