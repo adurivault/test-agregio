76c134246a47a74557c6e154d3596f0c381ba946
adurivault <a.durivault@gmail.com>
2024-07-17 09:51:14 +0200

    setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # test-agregio
+
+## Setup
+`docker compose up`
+- Create raw tables
+- Manually copy csv files to raw tables (can be done with pgadmin)
---
+++ sql/load_raw.sql
@@ -0,0 +1,33 @@
+CREATE TABLE raw_production (
+production_id SERIAL PRIMARY KEY,
+site_id INT NOT NULL,
+sensor_id INT NOT NULL,
+production_date DATE,
+energy_generated DECIMAL(10, 2),
+energy_type VARCHAR(50)
+);
+
+CREATE TABLE raw_sites (
+site_id SERIAL PRIMARY KEY,
+site_name VARCHAR(100),
+location VARCHAR(100),
+installation_date DATE,
+total_capacity DECIMAL(10, 2)
+);
+
+CREATE TABLE raw_sensors (
+sensor_id SERIAL PRIMARY KEY,
+site_id INT NOT NULL,
+sensor_type VARCHAR(50),
+installation_date DATE,
+status VARCHAR(50)
+);
+
+-- COPY raw_production(production_id, site_id, sensor_id, production_date, energy_generated, energy_type)
+-- FROM 'data/production_renewable.csv' DELIMITER ',' CSV HEADER;
+
+-- COPY raw_sites(site_id, site_name, location, installation_date, total_capacity)
+-- FROM 'data/sites_renewable.csv' DELIMITER ',' CSV HEADER;
+
+-- COPY raw_sensors(sensor_id, site_id, sensor_type, installation_date, status)
+-- FROM 'data/sensors_renewable.csv' DELIMITER ',' CSV HEADER;
